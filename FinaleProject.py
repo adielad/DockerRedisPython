ac85ca41b94a07c257fc7ee11949434e83f82006
adielad <elada242@gmail.com>
2019-04-28 18:44:52 +0300

    add print

--- FinaleProject.py
+++ FinaleProject.py
@@ -4,3 +4,5 @@ driverChrome = webdriver.Chrome(executable_path='C:/DevOps/Class 2/chromedriver_
 
 URL = "http://192.168.99.100:5000"
 driverChrome.get(URL)
+el=driverChrome.find_element_by_tag_name("body")
+print(el.text)
